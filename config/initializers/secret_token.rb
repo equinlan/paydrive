649fa09b5a0d53a0f337788b5ac4191e264d8f97
Evan Quinlan <evan.quinlan@gmail.com>
2014-02-10 22:30:48 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paydrive::Application.config.secret_key_base = '075a24616951ab9027265cfe521c56d2c8bf84a24c6259e65dcd7d59d4c78ccad2751bcadecda0707464c3ab621b3aa47d92db25b03c493d0752851ddfda3fe3'
