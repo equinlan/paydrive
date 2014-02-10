7f3cf0c75180aea630b9c7bfb24d64cc83ef6a8d
Evan Quinlan <evan.quinlan@gmail.com>
2014-02-10 23:08:48 +0000

    Try to add validation to returned message.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,7 +2,7 @@ class AccountController < ApplicationController
   protect_from_forgery except: :update
   
   def update
-    puts params
-    render nothing: true
+    params[:cmd] = _notify-validate
+    render params
   end
 end
